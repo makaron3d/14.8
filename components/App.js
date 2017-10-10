8b069985d12a672766853130dba91fcb8d27d502
makaron3d <makaron3d@gmail.com>
2017-10-10 12:20:34 +0200

    0002

--- components/App.js
+++ components/App.js
@@ -12,22 +12,22 @@ App = React.createClass({
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search />
-            <Gif />
+            <Gif
+    loading={this.state.loading}
+    url={this.state.gif.url}
+    sourceUrl={this.state.gif.sourceUrl}
+/>
           </div>
         );
     }
 });
 
-<Gif
-    loading={this.state.loading}
-    url={this.state.gif.url}
-    sourceUrl={this.state.gif.sourceUrl}
-/>
+
 
 getInitialState() {
     return {
         loading: false,
-        searchingText: '',
+        searchingText: 'gf',
         gif: {}
     };
 },
